ff621a2ec2572a0032040b65ea4406dd07a4aea4
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2019-01-08 14:18:07 +0100

    Fix bug in js client

--- grpc/jsclient.go
+++ grpc/jsclient.go
@@ -4,12 +4,9 @@ package grpc
 
 import (
 	"context"
-	"net/url"
-	"strings"
 	"sync"
 
 	log "github.com/sirupsen/logrus"
-	"honnef.co/go/js/dom"
 
 	"github.com/kyeett/gameserver/entity"
 	pbweb "github.com/kyeett/gameserver/proto/web"
@@ -27,35 +24,9 @@ type GrpcClient struct {
 	startedCh chan struct{} // Todo, better method for knowing if started?
 }
 
-var document = dom.GetWindow().Document().(dom.HTMLDocument)
-
-func init() {
-	document.AddEventListener("DOMContentLoaded", false, func(_ dom.Event) {
-		log.Error("Document loaded!")
-	})
-}
-
 func CorrectClient(serverAddr string, secure bool) (pbweb.BackendClient, error) {
-	u2, err := url.Parse(document.DocumentURI())
-	if err != nil {
-		log.Error("unexpected error parsing URI", err)
-		return nil, err
-	}
-
-	addr := "localhost:10001"
-	if u2.Query().Get("addr") != "" {
-		addr = u2.Query().Get("addr")
-
-		if !strings.Contains(addr, "http") {
-			addr = "https://" + addr
-		}
-	}
-
-	log.Printf("Connecting to %s\n", addr)
 
 	clas := pbweb.NewBackendClient(serverAddr)
-	document.Body().SetInnerHTML(`<div><h2>GopherJS gRPC-Web is great!</h2></div>`)
-
 	return clas, nil
 }
 
